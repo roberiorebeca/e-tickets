3162c03ccb5802699fb00f6266f07128df52eb20
Roberio <roberio@hifuzion.com.br>
2019-08-12 09:25:54 -0400

    #2 Implementado a Base do Sistema

---
+++ api/base/models.py
@@ -0,0 +1,57 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Empresa(models.Model):
+    """
+    Modelo para guardar dados da Empresa que vai usar o sistema
+    """
+    nome = models.CharField(max_length=150,
+                            verbose_name='Razão Social'
+                            )
+    telefone = models.CharField(max_length=14
+                                )
+    endereco = models.CharField(max_length=100,
+                                verbose_name='endereço'
+                                )
+    cidade = models.CharField(max_length=100,
+                              verbose_name='cidade/uf'
+                              )
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = 'empresa'
+        verbose_name_plural = 'empresas'
+
+
+class Modulos(models.Model):
+    """
+    Modelo para guardar os dados do módulo do sistema
+    """
+    descricao = models.CharField(max_length=100,
+                                 verbose_name='descrição'
+                                 )
+
+    def __str__(self):
+        return self.descricao
+
+    class Meta:
+        verbose_name = 'módulo'
+        verbose_name_plural = 'módulos'
+
+
+class Usuario(AbstractUser):
+    """
+    Modelo para vincular ao cadastro de Usuario do Django
+    """
+    cliente = models.BooleanField(default=False)
+    suporte = models.BooleanField(default=False,
+                                  verbose_name='suporte técnico'
+                                  )
+    desenvolvedor = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'usuário'
+        verbose_name_plural = 'usuários'
--- api/etickets/settings.py
+++ api/etickets/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,20 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+THIRD_APPS = []
+
+LOCAL_APPS = [
+    'base',
+    'cadastros',
+    'movimentos',
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_APPS + LOCAL_APPS
+
+# Custom
+AUTH_USER_MODEL = 'base.Usuario'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
