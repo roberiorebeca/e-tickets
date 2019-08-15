c43493b9b48af63456dde7a1082f8b6f81dfb276
Roberio <roberio@hifuzion.com.br>
2019-08-15 09:13:22 -0400

    Trabalhando mo teste da API

--- api/base/tests.py
+++ api/base/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
 
-from .models import Empresa, Modulo
+from .models import Empresa, Modulo, Usuario
 
 
 class TestEmpresa(TestCase):
@@ -36,3 +38,21 @@ class TestModulo(TestCase):
 
         self.assertEqual(str(mod), mod.descricao, 'Verifica __str__ do objeto')
         self.assertEqual(mod.pk, 1, 'Verifica se salvou no banco')
+
+
+class TesteModulo(TestCase):
+
+    def setUp(self) -> None:
+        """
+        Class para testar a inclusão do cadastro de Módulo
+        """
+        self.user = Usuario.objects.create_superuser('admin', 'admin@admin.com.br', '123ABCD')
+        self.api = APIClient()
+        self.api.force_authenticate(user=self.user)
+        self.url = '/base/'
+
+    def test_add_modulo(self):
+        url = f'{self.url}modulos/'
+        post = dict(descricao='Teste')
+        response = self.api.post(url, post, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
